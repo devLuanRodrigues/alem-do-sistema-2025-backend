4df96d89873da28a95a52ef20b82475781d99f15
Luan Rodrigues <rodriguesluandev@gmail.com>
2025-03-28 19:19:31 -0300

    add: search by name and cpf

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -22,3 +22,46 @@ function deleteClient(button) {
 function addContact(){
     alert('Função ainda não implementada!');
 }
+
+function searchClients() {
+    const name = document.getElementById("searchName").value;
+    const cpf = document.getElementById("searchCpf").value;
+
+    let url = '/clients/search?';
+    if (name) {
+        url += `nome=${encodeURIComponent(name)}&`;
+    }
+    if (cpf) {
+        url += `cpf=${encodeURIComponent(cpf)}&`;
+    }
+
+    fetch(url)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Erro ao buscar clientes');
+            }
+            return response.json();
+        })
+        .then(clients => {
+            const tableBody = document.getElementById("clientTableBody");
+            tableBody.innerHTML = '';
+
+            clients.forEach(client => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${client.cpf}</td>
+                    <td>${client.nome}</td>
+                    <td>${client.contato[0] ? client.contato[0].valorContato : 'N/A'}</td>
+                    <td>
+                        <a href="/clients/${client.id}" class="btn btn-primary">Perfil</a>
+                        <button class="btn btn-danger" th:data-client-id="${client.id}" onclick="deleteClient(this)">Excluir</button>
+</td>
+                `;
+                tableBody.appendChild(row);
+            })
+        })
+        .catch(error => {
+            console.error(error);
+            alert('Erro ao buscar clientes.');
+        });
+}
