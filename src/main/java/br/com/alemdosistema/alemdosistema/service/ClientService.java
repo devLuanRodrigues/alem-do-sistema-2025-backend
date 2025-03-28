ffe0b6a364e046bc7d108a6a9da6900b960188e7
Luan Rodrigues <rodriguesluandev@gmail.com>
2025-03-28 08:36:38 -0300

    feat: header template, index/home page and HomeController

--- src/main/java/br/com/alemdosistema/alemdosistema/service/ClientService.java
+++ src/main/java/br/com/alemdosistema/alemdosistema/service/ClientService.java
@@ -9,7 +9,6 @@ import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
